3b397ae8814d51854d1356022cc2c985ceb00342
Rohit-singhal-crypto <rohityadavrohit8361@gmail.com>
2025-04-29 10:19:02 +0530

    new code

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,14 @@
 import Alltags from "@/components/Alltags";
-import Footer from "@/components/Common/footer";
 import Navbar from "@/components/Common/Navbar";
 import Home from "@/components/Home";
-import Htmllayout from "@/components/Htmllayout";
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <Navbar />
-      <Home/>
-      {/* <Htmllayout/> */}
+      <Home />
       <Alltags />
-      <Footer />
     </div>
   );
 };
